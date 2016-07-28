ca4a4f4edbfb3a4f06c18304850440ad238cfae8
areph <40balmung@gmail.com>
2016-07-28 20:56:54 +0900

    Add vim packege manager

--- setup.sh
+++ setup.sh
@@ -8,7 +8,13 @@ do
     ln -s $HOME/dotfiles/$f $HOME/$f
 done
 
+## Other setup
+
+## vim
+# color
 git clone git://github.com/altercation/vim-colors-solarized.git
 mkdir ~/.vim 1>/dev/null 2>/dev/null
 cp -pr vim-colors-solarized/colors/ ~/.vim/
 
+# dein
+cp -pr rc/ ~/.vim/
