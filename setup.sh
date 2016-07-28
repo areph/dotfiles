271d2668f5ff38b63cdbe2923ec9e139ebc84b01
areph <40balmung@gmail.com>
2016-07-28 11:22:56 +0000

    Add vim color settings

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ do
 
     ln -s $HOME/dotfiles/$f $HOME/$f
 done
+
+git clone git://github.com/altercation/vim-colors-solarized.git
+mkdir ~/.vim 1>/dev/null 2>/dev/null
+cp -pr vim-colors-solarized/colors/ ~/.vim/
+
